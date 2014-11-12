0b92b39e970e8b1c839d399c89f60d01f023c419
Christoph Holtermann <c.holtermann@gmx.de>
2014-11-12 21:58:44 +0100

    forgotten file

---
+++ src/optional/python-bindings/example_scripts/gncCompany.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+class Company(object):
+    """Information about the Company that owns the book."""
+    def __init__(self, book):
+        self.book = book
+        self.slots = self.book.get_slots()
+
+    def GetName(self):
+        return self.slots.get_string("options/Business/Company Name")
+
+    def GetAddress(self):
+        return self.slots.get_string("options/Business/Company Address")
+
+    def GetID(self):
+        return self.slots.get_string("options/Business/Company ID")
+
+    def GetPhone(self):
+        return self.slots.get_string("options/Business/Company Phone Number")
+
+    def GetFax(self):
+        return self.slots.get_string("options/Business/Company Fax Number")
+
+    def GetWebsite(self):
+        return self.slots.get_string("options/Business/Company Website URL")
+
+    def GetEmail(self):
+        return self.slots.get_string("options/Business/Company Email Address")
+
+    def GetContactPerson(self):
+        return self.slots.get_string("options/Business/Company Contact Person")
