392344ac4fab0fd5ef601e2254325c96958b1c79
Christoph Holtermann <c.holtermann@gmx.de>
2018-08-28 19:00:46 +0200

    put import statement where it is needed

--- bindings/python/gnucash_core.py
+++ bindings/python/gnucash_core.py
@@ -367,8 +367,6 @@ class GncNumeric(GnuCashCoreClass):
 
         return forward, reverse
 
-    import operator
-
     def _add(a, b):
         return a.add(b, GNC_DENOM_AUTO, GNC_HOW_RND_ROUND_HALF_UP)
 
@@ -380,6 +378,8 @@ class GncNumeric(GnuCashCoreClass):
 
     def _div(a, b):
         return a.div(b, GNC_DENOM_AUTO, GNC_HOW_RND_ROUND_HALF_UP)
+
+    import operator
 
     __add__, __radd__ = _operator_fallbacks(_add, operator.add)
     __iadd__ = __add__
