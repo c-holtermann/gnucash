e5a87007932f243605625a2c6f267cb36c3c6aaa
c-holtermann <mail@c-holtermann.net>
2021-05-22 16:56:04 +0200

    make main docstring a little more informative

--- bindings/python/__init__.py
+++ bindings/python/__init__.py
@@ -1,3 +1,11 @@
+# Python bindings for gnucash
+#
+# to get started have a look at
+# * the example files
+# * doxygen source docs (https://lists.gnucash.org/docs/MAINT/python_bindings_page.html)
+# * wiki (https://wiki.gnucash.org/wiki/Python_Bindings)
+# * source documentation (in some places rather sparse)
+#
 # import all the symbols from gnucash_core, so basic gnucash stuff can be
 # loaded with:
 # >>> from gnucash import thingy
