13641ac2da7445002005ae9c3622b822c7b38ff8
c-holtermann <mail@c-holtermann.net>
2021-05-22 16:58:57 +0200

    reorder comments

--- bindings/python/__init__.py
+++ bindings/python/__init__.py
@@ -11,11 +11,13 @@
 # >>> from gnucash import thingy
 # instead of
 # >>> from gnucash.gnucash_core import thingy
-from gnucash.gnucash_core import *
-from . import app_utils
-from . import deprecation
+#
 ##  @file
 #   @brief helper file for the importing of gnucash
 #   @author Mark Jenkins, ParIT Worker Co-operative <mark@parit.ca>
 #   @author Jeff Green,   ParIT Worker Co-operative <jeff@parit.ca>
 #   @ingroup python_bindings
+
+from gnucash.gnucash_core import *
+from . import app_utils
+from . import deprecation
